c31f27b65a08d8da8645bc1e7a7b1dfa1421ddb6
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-04-29 12:41:19 +0300

    NewSolutionand Prjects

---
+++ B22 Ex03 Amir 208423491 Roni 322437815/ConsoleUI/GarageManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GarageLogic;
+
+namespace ConsoleUI
+{
+    class GarageManager
+    {
+       private readonly Garage r_Garge;
+    }
+}
---
+++ B22 Ex03 Amir 208423491 Roni 322437815/GarageLogic/Garage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GarageLogic
+{
+    public class Garage
+    {
+        private readonly VehicleManufactor r_vehicleManufactor = new VehicleManufactor(); 
+    }
+}
